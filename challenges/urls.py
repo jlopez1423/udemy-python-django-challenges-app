d45ed600f9cc22567b321127636bdcb63530200a
Jose Lopez <jalopez1423@gmail.com>
2023-04-11 00:40:49 -0500

    refactored to use dynamic url patterns

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
  
 urlpatterns = [
-    path("january", views.january),
-    path("february", views.feburary)   
+    path("<month>", views.monthly_challenge)
 ]
