f11884950eaabc9b45746fb8865c5b81d461158b
Jose Lopez <jalopez1423@gmail.com>
2023-07-10 21:32:24 -0500

    Added dynamic url names. Added HTML as a response

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 monthly_challenges = {
@@ -24,11 +25,13 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Invalid month!")
 
     redirect_month = months[month-1]
+    redirect_path = reverse("month-challenge", args=[redirect_month]) # /challenge/january
 
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    return HttpResponseRedirect(redirect_path)
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
